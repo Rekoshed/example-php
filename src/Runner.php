cc183af2c4043c81e5158936d5f406094b0276c7
rekoshed <rekoshed@yandex.ru>
2019-11-13 01:24:55 -0400

    create first packages

---
+++ src/Runner.php
@@ -0,0 +1,12 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+namespace My\Project\Runner;
+
+function run()
+{
+    $collection = collect(['taylor', 'abigail', null])->map(function ($name) {
+        return strtoupper($name);
+    });
+
+    return $collection;
+}
